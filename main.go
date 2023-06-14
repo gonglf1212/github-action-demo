c64cd2015b31818ca4fb409525af576e205e9e31
gonglingfeng <you@example.com>
2023-06-14 16:17:47 +0800

    demo

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"net/http"
+)
 
 func main() {
-	fmt.Println("go项目启动了！！")
+
+	fmt.Println("go服务启动了，启动时间：", time.Now().Format("2006-1-2 15:04:05"))
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
