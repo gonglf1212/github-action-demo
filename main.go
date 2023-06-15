d2f08b01645795436df50233b0bc105c5c72bffe
gonglingfeng <you@example.com>
2023-06-15 18:03:13 +0800

    cron

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ import (
 func main() {
 
 	fmt.Println("go服务启动了，启动时间：", time.Now().Format("2006-1-2 15:04:05"))
-	HttpGet()
+	HttpGet() //他可以访问一个接口
 }
 
 func HttpGet() {
-	resp, err := http.Get("http://baidu.com/")
+	resp, err := http.Get("https://www.baidu.com/")
 	if err != nil {
 		// 处理错误
 		panic(err)
