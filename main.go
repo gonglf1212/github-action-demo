2d907e1e40878019d9057b70c203a3612f4f9333
gonglingfeng <you@example.com>
2023-06-15 17:22:21 +0800

    fix

--- main.go
+++ main.go
@@ -11,8 +11,18 @@ import (
 func main() {
 
 	fmt.Println("go服务启动了，启动时间：", time.Now().Format("2006-1-2 15:04:05"))
+	RunHttp()
+}
+
+func HttpGet() {
+
+}
+
+// Http接口估计外面访问不了，死心了
+func RunHttp() {
 	http.HandleFunc("/", helloHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
